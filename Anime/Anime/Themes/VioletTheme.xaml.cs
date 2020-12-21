1cd825ee0661db4c1e2eeae0750f1a8448fd1c7f
eugene-zhary <eugenezhary05@gmail.com>
2020-12-22 01:45:21 +0200

    create themes

---
+++ Anime/Anime/Themes/VioletTheme.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Anime.Themes
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VioletTheme : ResourceDictionary
+    {
+        public VioletTheme()
+        {
+            InitializeComponent();
+        }
+    }
+}
