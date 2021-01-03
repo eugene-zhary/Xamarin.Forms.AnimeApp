3e063834f11e581a4c6a68b549f302e2c4950aed
eugene-zhary <eugenezhary05@gmail.com>
2021-01-03 17:33:46 +0200

    color settings view added

---
+++ Anime/Anime/ViewModels/ColorSettingsViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Anime.Navigable;
+
+namespace Anime.ViewModels
+{
+    public class ColorSettingsViewModel: BaseViewModel
+    {
+        private double hueValue;
+        public double HueValue {
+            get => hueValue;
+            set {
+                if (hueValue != value) {
+                    Set(ref hueValue, value);
+                    App.SetHue(Math.Round(value: hueValue, digits: 2));
+                }
+            }
+        }
+
+        public ColorSettingsViewModel()
+        {
+            this.hueValue = App.GetHue();
+        }
+
+
+    }
+}
--- Anime/Anime/ViewModels/SettingsVM/SettingsViewModel.cs
+++ Anime/Anime/ViewModels/SettingsVM/SettingsViewModel.cs
@@ -8,21 +8,11 @@ namespace Anime.ViewModels
 {
     public class SettingsViewModel : ANavigableViewModel
     {
-        private double hueValue;
-        public double HueValue {
-            get => hueValue;
-            set {
-                if(hueValue != value) {
-                    Set(ref hueValue, value);
-                    App.SetHue(Math.Round(value: hueValue, digits: 2));
-                }
-            }
-        }
+        public ColorSettingsViewModel ColorSettingsViewModel { get; set; }
 
         public SettingsViewModel(INavigationService navigationService) : base(navigationService)
         {
-            this.hueValue = App.GetHue();
+            this.ColorSettingsViewModel = new ColorSettingsViewModel();
         }
-
     }
 }
